d518d0e9a68a485eb48881397e53a83f89e16788
felipeluis222000 <luis.felipe.santiago230199@gmail.com>
2021-08-16 15:56:27 -0300

    16/08/2021

--- main.py
+++ main.py
@@ -8,13 +8,31 @@ LARGURA_GRADE = 300
 ALTURA_GRADE = 600
 TOPO_X = (LARGURA/2)-(LARGURA_GRADE/2)
 TOPO_Y = 50
+FONTES = ["aerobusdotty.ttf", "THE WAVE.ttf", "Cater.ttf",
+          "Fashionable.ttf","Ghost Of The Wild West.ttf", "MIDELTONEROUGH.ttf", "Nuvel.ttf",
+          "Qaitan Serif Font.ttf", "Stthomas Script.ttf"]
 
 
 
 pygame.font.init()
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode((LARGURA, ALTURA))
-pygame.display.set_caption("Tetris")
+pygame.display.set_caption("Tetrinho")
+
+def Bloquinhos(x,y):
+    formatos1 = [[(x,y),(x+30,y),(x,y+30),(x+30,y+30),(255,255,255)],
+                 (x,y),(x+30,y),(x,y+30),(x+30,y+30),(255,255,255)]
+
+    formatos2 = [[(x,y),(x,y+30),(x,y+60),(x,y+90), (255,255,255)],
+                 [(x,y),(x-30,y),(x+30,y),(x+60,y), (255,255,255)]]
+    return formatos2
+
+def Titulo(fontes):
+    cor = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    fonte = random.choice(fontes)
+    titulo = pygame.font.Font("fontes/{}".format(fonte), 50)
+    titulo2 = titulo.render("Tetrinho", 1, (cor))
+    return titulo2
 
 def Mapa(tela, x, y, largura, altura):
     for i in range(10):
@@ -23,7 +41,31 @@ def Mapa(tela, x, y, largura, altura):
         pygame.draw.line(tela, (128,128,128), (x,y+(i*30)), (x+300,y+(i*30)))
     pygame.draw.rect(tela, (72,61,139), (x,y, largura, altura), 3)
 
+def Movimentacao(bloquinho, sentido):
+    final = []
+    if sentido == "baixo":
+        for i in range(len(bloquinho)-1):
+            posicao = list(bloquinho[i])
+            final.append((posicao[0],posicao[1]+30))
+        final.append(bloquinho[4])
+        return final
+
+    elif sentido == "direita":
+        for i in range(len(bloquinho)-1):
+            posicao = list(bloquinho[i])
+            final.append((posicao[0]+30,posicao[1]))
+        final.append(bloquinho[4])
+        return final
+
+    elif sentido == "esquerda":
+        for i in range(len(bloquinho)-1):
+            posicao = list(bloquinho[i])
+            final.append((posicao[0]-30,posicao[1]))
+        final.append(bloquinho[4])
+        return final
+
 def Main(tela):
+    global FONTES
     global LARGURA
     global ALTURA
     global LARGURA_GRADE
@@ -32,31 +74,33 @@ def Main(tela):
     global TOPO_Y
 
     tamanho = 30
-    x = (LARGURA/2)-(tamanho/2)
-    y = 50
     contador_segundos = 0
     rodando = True
-
+    titulo = Titulo(FONTES)
+    bloquinho = Bloquinhos(TOPO_X+120,TOPO_Y)
     while rodando:
         clock.tick(30)
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 rodando = False
-                return False
+                return rodando
             if evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_s:
-                    y += 30
+                    bloquinho = Movimentacao(bloquinho, "baixo")
                 if evento.key == pygame.K_d:
-                    x += 30
+                    bloquinho = Movimentacao(bloquinho, "direita")
                 if evento.key == pygame.K_a:
-                    x -= 30
+                    bloquinho = Movimentacao(bloquinho, "esquerda")
 
         if contador_segundos == 30:
-            y += 30
+            bloquinho = Movimentacao(bloquinho, "baixo")
+            titulo = Titulo(FONTES)
             contador_segundos = 0
 
         tela.fill((0, 0, 0))
-        pygame.draw.rect(tela, (127,0,0), ((x-(tamanho/2)), y, 30, 30), 0)
+        tela.blit(titulo, (TOPO_X + (TOPO_X/2)-(titulo.get_width()/2),5))
+        for i in range(len(bloquinho)-1):
+            pygame.draw.rect(tela, bloquinho[4],(bloquinho[i][0],bloquinho[i][1], tamanho, tamanho),0)
         Mapa(tela, TOPO_X, TOPO_Y, LARGURA_GRADE, ALTURA_GRADE)
         pygame.display.flip()
         contador_segundos = contador_segundos + 1
